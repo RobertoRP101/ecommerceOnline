a9599a6c7c56194a2c8f4753dfa99ffd86f6f9c1
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-04 23:47:01 -0600

    Media file configuration

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -128,3 +128,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media file configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
