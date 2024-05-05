8d3bffc10013218a27309009a6cbd7dc4f421bb7
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-04 23:05:03 -0600

    Superuser parameters

--- accounts/models.py
+++ accounts/models.py
@@ -22,7 +22,13 @@ class MyAccountManager(BaseUserManager):
         return user
     
     def create_superuser(self, first_name, last_name, email, username, password):
-        pass
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
 
 
 class Account(AbstractBaseUser):
