3d68615b7be1fce6fce52fbcaa4ef38413e78242
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-04 23:34:16 -0600

    AccountAdmin rules

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,9 @@ from .models import Account
 
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
     
 
 admin.site.register(Account, AccountAdmin)
