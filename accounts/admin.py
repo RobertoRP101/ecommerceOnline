93730f7fa0541bb9bf4a460cb1bf972763e15bbb
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-04 23:33:05 -0600

    AccountAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active',)
+    
+
+admin.site.register(Account, AccountAdmin)
