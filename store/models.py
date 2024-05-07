32ab4307251dce53e0b3d8da1dc2009b9984eca7
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-06 21:54:37 -0600

    Product class

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
