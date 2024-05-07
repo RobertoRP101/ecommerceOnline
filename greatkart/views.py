436fbb1ffe5e7586eecffd1185a2cdef5eea3eb9
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-06 23:37:30 -0600

    Products to display

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from store.models import Product
 
 def home(request):
+    products = Product.objects.all().filter(is_available=True)
     return render(request, 'home.html')
