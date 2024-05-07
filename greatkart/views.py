f5fe962f61ab14e6d7aafdf3b8168953556d26c1
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-06 23:40:25 -0600

    Remove static product from the home page

--- greatkart/views.py
+++ greatkart/views.py
@@ -3,4 +3,7 @@ from store.models import Product
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
-    return render(request, 'home.html')
+    context = {
+        'products': products,
+    }
+    return render(request, 'home.html', context)
