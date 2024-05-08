4a9ae6fd92b09031bda6df06660de86bd5bb84ac
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-07 23:41:05 -0600

    Include store urls to greatkar urls

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),   
+    path('', views.home, name='home'),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),   
+    path('', views.home, name='store'),   
 ] 
