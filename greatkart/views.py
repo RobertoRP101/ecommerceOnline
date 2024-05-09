fd92fb427bf83036ba6774b92f738e616cce5c0b
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-08 23:59:01 -0600

    Display products in store page. Some bugs fixed

--- greatkart/views.py
+++ greatkart/views.py
@@ -3,9 +3,7 @@ from store.models import Product
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
     context = {
         'products': products,
-        'product_count': product_count,
     }
     return render(request, 'home.html', context)
--- store/views.py
+++ store/views.py
@@ -4,7 +4,9 @@ from .models import Product
 
 def store(request):
     products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
     context = {
         'products': products,
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
