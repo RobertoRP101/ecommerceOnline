06b406bd6c53947fc39735050f3f71ec86aae14c
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-09 23:26:13 -0600

    Category_slug paramter at store function

--- store/views.py
+++ store/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from category.models import Category
 from .models import Product
 # Create your views here.
 
-def store(request):
+def store(request, category_slug=None):
+    categories = None
+    products = None
+    
+    if category_slug != None:
+        categories = get_object_or_404(Category, category_slug=slug) 
+    
     products = Product.objects.all().filter(is_available=True)
     product_count = products.count()
     context = {
