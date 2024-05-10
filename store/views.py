075c7fa4004bd410207df4fa904e73484c3ea759
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-09 23:33:25 -0600

    Display products by category

--- store/views.py
+++ store/views.py
@@ -9,10 +9,12 @@ def store(request, category_slug=None):
     products = None
     
     if category_slug != None:
-        categories = get_object_or_404(Category, category_slug=slug) 
+        categories = get_object_or_404(Category, slug=category_slug) 
         products = Product.objects.filter(category=categories, is_available=True)
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+        product_count = products.count()
+    else:  
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
     context = {
         'products': products,
         'product_count': product_count,
