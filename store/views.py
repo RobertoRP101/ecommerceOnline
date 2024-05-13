091edea1e6ff6d94cea5b24c7cfaa98bf81b9700
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-12 23:45:33 -0600

    Parameters

--- store/views.py
+++ store/views.py
@@ -22,5 +22,5 @@ def store(request, category_slug=None):
     return render(request, 'store/store.html', context)
 
 
-def product_detail(request):
+def product_detail(request, category_slug, product_slug):
     return render(request, 'store/product_detail.html')
