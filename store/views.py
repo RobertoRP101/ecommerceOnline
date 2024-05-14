073f14f379b71ca70c293fdbf537af3825d80f4a
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-13 23:53:04 -0600

    Context argument

--- store/views.py
+++ store/views.py
@@ -27,4 +27,7 @@ def product_detail(request, category_slug, product_slug):
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
     except Exception as e:
         raise e
-    return render(request, 'store/product_detail.html')
+    context = {
+        'single_product': single_product, 
+    }
+    return render(request, 'store/product_detail.html', context)
