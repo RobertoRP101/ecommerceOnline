6e9535e42fa78f2fa568830b3f0d9dd2bb5ff8ef
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-19 23:57:14 -0600

    Magic function str

--- carts/models.py
+++ carts/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Cart(models.Models):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
