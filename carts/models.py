509de1bc9024b8785e8a53300001058805a808bf
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-20 23:32:14 -0600

    is_activate variable

--- carts/models.py
+++ carts/models.py
@@ -15,3 +15,4 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
