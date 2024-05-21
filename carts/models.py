1c454ebc98f47678931b006b9cfd24771770fafb
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-20 23:47:01 -0600

    Model instead Models

--- carts/models.py
+++ carts/models.py
@@ -3,7 +3,7 @@ from store.models import Product
 
 # Create your models here.
 
-class Cart(models.Models):
+class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
