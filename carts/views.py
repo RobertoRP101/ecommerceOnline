5e6627b9924df0c673ccb4731233f9d46141c82e
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-21 14:48:04 -0600

    add_cart function

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
 
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
 
 def cart(request):
     return render(request, 'store/cart.html')
