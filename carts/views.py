600b5fde6413f54af9937be7e83643721859f2f8
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-21 18:04:07 -0600

    Partial functionality of add_cart and cart_id function signature

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from store.models import Product
-
+from .models import Cart
 # Create your views here.
 
+def cart_id():
+    pass
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get()
+    except:
+        pass
 
 def cart(request):
     return render(request, 'store/cart.html')
