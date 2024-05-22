1dc5b7d114a56a31a0827ce0017326bf44b877ec
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-21 18:30:04 -0600

    Getting cart object based on cart_id function

--- carts/views.py
+++ carts/views.py
@@ -3,13 +3,17 @@ from store.models import Product
 from .models import Cart
 # Create your views here.
 
-def cart_id():
-    pass
+def cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+    
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
-        cart = Cart.objects.get()
+        cart = Cart.objects.get(id_cart=cart_id(request))
     except:
         pass
 
