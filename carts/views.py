2a8e777097d8356f59ec1ed8430a5eafde6dfdbc
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-22 23:43:52 -0600

    total and quantity variables defined

--- carts/views.py
+++ carts/views.py
@@ -37,4 +37,7 @@ def add_cart(request, product_id):
 def cart(request, total=0, quantity=0, cart_items=None):
     cart = Cart.objects.get(cart_id=cart_id(request))
     cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
     return render(request, 'store/cart.html')
