172cf0151f38706d116ea66322b5677e4c1d18d7
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-24 22:43:25 -0600

    Tax variable

--- carts/views.py
+++ carts/views.py
@@ -41,6 +41,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total)/100
     except ObjectNotExist:
         pass
     
