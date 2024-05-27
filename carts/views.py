ffa5f71574d7e3746a5b7d4d6b6d3c5880af614a
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-27 14:31:35 -0600

    cart_id instead of id_cart

--- carts/views.py
+++ carts/views.py
@@ -5,7 +5,7 @@ from .models import Cart, CartItem
 
 # Create your views here.
 
-def cart_id(request):
+def _cart_id(request):
     cart = request.session.session_key
     if not cart:
         cart = request.session.create()
@@ -15,10 +15,10 @@ def cart_id(request):
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
-        cart = Cart.objects.get(id_cart=cart_id(request))
+        cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
         cart = Cart.objects.create(
-            cart_id = cart_id(request)
+            cart_id = _cart_id(request)
         )
     cart.save()
     try:
@@ -36,7 +36,7 @@ def add_cart(request, product_id):
     
     
 def remove_cart(request, product_id):
-    cart = Cart.objects.get(cart_id=cart_id(request))
+    cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
     cart_item = CartItem.objects.get(product=product, cart=cart)
     if cart_item.quantity > 1:
@@ -48,8 +48,9 @@ def remove_cart(request, product_id):
 
 
 def cart(request, total=0, quantity=0, cart_items=None):
+    tax = grand_total = 0
     try:
-        cart = Cart.objects.get(cart_id=cart_id(request))
+        cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
