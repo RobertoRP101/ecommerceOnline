bb5d280671e0c5399c6f4c2e20d582d3f401f846
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-26 23:43:59 -0600

    if-else block

--- carts/views.py
+++ carts/views.py
@@ -38,6 +38,12 @@ def remove_cart(request, product_id):
     cart = Cart.objects.get(cart_id=cart_id(request))
     product = get_object_or_404(Product, id=product_id)
     cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
 
 
 def cart(request, total=0, quantity=0, cart_items=None):
