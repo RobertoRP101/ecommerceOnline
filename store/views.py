b8a75e2a20c778ba3aeb5aa553dd60a0dc62106a
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-05-30 23:46:58 -0600

    paged_products variable

--- store/views.py
+++ store/views.py
@@ -18,6 +18,7 @@ def store(request, category_slug=None):
         products = Product.objects.all().filter(is_available=True)
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     context = {
         'products': products,
