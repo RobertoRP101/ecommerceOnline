86eeef8cf1f45560935c12fa28d2ffa81ac853e9
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-02 22:56:45 -0600

    task installed apps to check the set up

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task', # Verify if the settings were set up correctly -- python manage.py makemigrations tasks
     'category',
     'accounts',
     'store',
