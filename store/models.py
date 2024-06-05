53999f958fa1b8c262f29cf6c964b70af249fce7
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-04 23:56:29 -0600

    variation_value, is_active and created_date attributes

--- store/models.py
+++ store/models.py
@@ -29,3 +29,6 @@ variation_category_choice = (
 class Variation(models.Model):
         product = models.ForeignKey(Product, on_delete=models.CASCADE)
         variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+        variation_value = models.CharField(max_length=100)
+        is_active = models.BooleanField(default=True)
+        created_date = models.DateTimeField(auto_now=True)
