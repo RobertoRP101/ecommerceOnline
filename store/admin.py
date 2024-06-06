e6df95f9849a3d2bfdf5bf76b7c9a1d8af0495b4
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-05 23:38:02 -0600

    variationAdmin in admin file

--- store/admin.py
+++ store/admin.py
@@ -7,5 +7,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'created_date')
+
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Variation)
+admin.site.register(Variation, VariationAdmin)
