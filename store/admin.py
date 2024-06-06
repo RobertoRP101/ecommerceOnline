ee9616a73b9cf8a7d57ff3cde05cebdc65a0277e
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-05 23:41:28 -0600

    filter created by some items

--- store/admin.py
+++ store/admin.py
@@ -4,12 +4,13 @@ from .models import Product, Variation
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'is_available')
+    list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'is_available',)
     prepopulated_fields = {'slug': ('product_name',)}
 
 class VariationAdmin(admin.ModelAdmin):
-    list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'created_date')
-    list_editable = ('is_active')
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'created_date',)
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value',)
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
