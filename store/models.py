d4c2c4bedbe487a36623c7eda953541c08b56ec0
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-06 23:23:50 -0600

    filter by variation category (size)

--- store/models.py
+++ store/models.py
@@ -25,6 +25,9 @@ class VariationManager(models.Manager):
     def color(self):
         return super(VariationManager, self).filter(variation_category='color', is_active=True)
     
+    def size(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+    
 variation_category_choice = (
     ('color', 'color'),
     ('size', 'size'),
