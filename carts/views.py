c50de8754b4c7d8882042ac25832957313fa3939
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-07 23:49:26 -0600

    loop key and value

--- carts/views.py
+++ carts/views.py
@@ -13,9 +13,10 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     if request.method == 'POST':
-        color = request.POST['color']
-        size = request.POST['size']
-        print(color, size)
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            print(key, value)
     
     product = Product.objects.get(id=product_id)
     try:
