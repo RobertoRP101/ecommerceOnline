e0de6347b2e97a01cf4558a5d3305c9fc291cc0b
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-08 23:52:24 -0600

    cartItemAdmin class changed

--- carts/admin.py
+++ carts/admin.py
@@ -6,8 +6,8 @@ from .models import Cart, CartItem
 class CartAdmin(admin.ModelAdmin):
     list_display = ('cart_id', 'date_added')
 
-# class CartItemAdmin(admin.ModelAdmin):
-#     list_display = ('product', 'quantity', 'cart')
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'is_active')
 
 admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem)
