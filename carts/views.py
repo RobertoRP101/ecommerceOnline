3edb1f60b193eae28fbc2dca837060b988140d42
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-09 23:30:58 -0600

    variation clean

--- carts/views.py
+++ carts/views.py
@@ -38,6 +38,7 @@ def add_cart(request, product_id):
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
         if len(product_variation) > 0:
+            cart_item.variations.clear()
             for item in product_variation:
                 cart_item.variations.add(item)
         cart_item.quantity += 1
@@ -49,6 +50,7 @@ def add_cart(request, product_id):
             cart = cart,
         )
         if len(product_variation) > 0:
+            cart_item.variations.clear()
             for item in product_variation:
                 cart_item.variations.add(item)
         cart_item.save()
