c83588f4a1a11f19bd083c1e76b48850991ed7bb
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-13 23:27:46 -0600

    url changes

--- carts/views.py
+++ carts/views.py
@@ -59,7 +59,7 @@ def add_cart(request, product_id):
             if len(product_variation) > 0:
                 item.variations.clear()
                 for item in product_variation:
-                    cart_item.variations.add(product_variation)
+                    cart_item.variations.add(*product_variation)
             item.save()
     else:
         cart_item = CartItem.objects.create(
@@ -69,7 +69,7 @@ def add_cart(request, product_id):
         )
         if len(product_variation) > 0:
             cart_item.variations.clear()
-            cart_item.variations.add(product_variation)
+            cart_item.variations.add(*product_variation)
         cart_item.save()
     return redirect('cart')
     
@@ -88,10 +88,10 @@ def remove_cart(request, product_id, cart_item_id):
         pass
     return redirect('cart')
 
-def remove_cart_item(request, product_id):
+def remove_cart_item(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
 
