fe5ce94a9eb0584e38fca6890e416caea72c7940
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-14 23:42:36 -0600

    context and form in register function

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .forms import RegistrationForm
 # Create your views here.
 def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
     return render(request, 'accounts/register.html')
 
 def login(request):
