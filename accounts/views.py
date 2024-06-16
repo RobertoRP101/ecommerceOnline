ae26e0f5a4a9a91ee1279f271359324622028f35
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-15 23:40:47 -0600

    cleaning data and filling each field

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .models import Account
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password )
     form = RegistrationForm()
     context = {
         'form': form,
