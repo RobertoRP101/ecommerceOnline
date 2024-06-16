1ba5d74256667e255b15505e3cc15c32ae305c05
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-15 23:26:59 -0600

    placeholder for each field on register page

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,5 +15,9 @@ class RegistrationForm(forms.ModelForm):
         
     def __init__(self, *args, **kwargs):
         super(RegistrationForm).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Adress'
         for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'fomr-control'
+            self.fields[field].widget.attrs['class'] = 'form-control'
