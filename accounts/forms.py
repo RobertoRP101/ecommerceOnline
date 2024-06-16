272a6165cffe269d1639498a70af60f68afa9bfe
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-15 22:53:15 -0600

    attribute to each field on register page

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,8 @@ from .models import Account
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter Password'
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
     }))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Confirm Password'
@@ -11,3 +12,8 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+        
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'fomr-control'
