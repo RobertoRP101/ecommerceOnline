6e11297dab56c6aa0ca50b455ae809c9fa3f34d1
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-15 22:38:10 -0600

    password field configuration

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,9 @@ from  django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password'
+    }))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
