43d9a486e1b7a98b8db0d307fb4892171f14b54b
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-16 23:36:12 -0600

    django messages

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -136,3 +136,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media file configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
