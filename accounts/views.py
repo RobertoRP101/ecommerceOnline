bdb48d4c6886ebdb1f2e7354a3a3916c2996fac4
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-19 23:44:15 -0600

    parameters for message

--- accounts/views.py
+++ accounts/views.py
@@ -21,9 +21,16 @@ def register(request):
             
             #USER ACTIVATION
             current_site = get_current_site(request)
-            mail_subject = 'Please activate your account'
-            messages = render_to_string('accounts/account_verification_email.html')
-            
+            email_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(forces_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(email_subject,message, to=[to_email])
+            send_email.send()
             messages.success(request, 'Registration successful.')
             return redirect('register')
     else:
