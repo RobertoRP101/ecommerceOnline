1d439f42421364fef4d804d1d47144e7eaf15072
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-24 15:52:59 -0600

    enhancing the assignment user to cart

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
 
 # Verification email
 from django.contrib.sites.shortcuts import get_current_site
@@ -55,6 +57,17 @@ def login(request):
         password = request.POST['password']
         user = auth.authenticate(email=email, password=password)
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    print(cart_item)
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
             return redirect('dashboard')
