09d66d4b9171cbd6f9d5196022dff8e88fa38c6e
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-26 00:36:32 -0600

    checking if the user is authenticated in remove function

--- carts/views.py
+++ carts/views.py
@@ -123,10 +123,14 @@ def add_cart(request, product_id):
     
     
 def remove_cart(request, product_id, cart_item_id):
-    cart = Cart.objects.get(cart_id=_cart_id(request))
+    
     product = get_object_or_404(Product, id=product_id)
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if request.user.is_authenticated:
+            cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
         if cart_item.quantity > 1:
             cart_item.quantity -= 1
             cart_item.save()
