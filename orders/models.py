2c085e3b6f9f920db588e1270c5ec7714f768ebd
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-28 01:14:00 -0600

    orderproduct class, fields and magic function str

--- orders/models.py
+++ orders/models.py
@@ -45,3 +45,22 @@ class Order(models.Model):
     
     def __str__(self) -> str:
         return self.user.first_name
+    
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation = models.ForeignKey(Variation, on_delete=models.CASCADE)
+    color = models.CharField(max_length=50)
+    size = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    product_price = models.FloatField()
+    ordered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.product.product_name
+    
+    
