eb19f479c68c95f81b49deedc21c0eada2b30824
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-28 01:06:20 -0600

    magic fuction str in order class

--- orders/models.py
+++ orders/models.py
@@ -43,5 +43,5 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-    
-    
+    def __str__(self) -> str:
+        return self.user.first_name
