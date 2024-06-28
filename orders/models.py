81cff9f38ea65470ce021da58d6e43f701fe3a63
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-28 01:04:13 -0600

    fields of order class

--- orders/models.py
+++ orders/models.py
@@ -22,4 +22,26 @@ class Order(models.Model):
         ('Cancelled', 'Cancelled'),
     )
     
+    user = models.ForeignKey(Account, on_delete=models.SET_DEFAULT, null=True)
+    payment = models.ForeignKey(Payment, on_delete=models.SET_DEFAULT, blank=True, null=True)
+    order_number = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    address_line_1 = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    order_note = models.CharField(max_length=100, blank=True)
+    order_total = models.FloatField()
+    tax = models.FloatField()
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=20)
+    is_ordered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
     
