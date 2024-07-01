68fe882e273b18ffa65e1801db5465aaf777ee49
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-01 13:57:26 -0600

    generate order number

--- orders/views.py
+++ orders/views.py
@@ -3,15 +3,23 @@ from django.shortcuts import render, redirect
 from carts.models import CartItem
 from .forms import OrderForm
 from .models import Order
+import datetime
 
-
-def place_order(request):
+def place_order(request, total=0, quantity=0,):
     current_user = request.user
     
     cart_items = CartItem.objects.filter(user=current_user)
     cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('store')
+    
+    grand_total = 0
+    tax = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total)/100
+    grand_total = total + tax
     
     if request.method == 'POST':
         form = OrderForm(request.POST)
@@ -27,4 +35,14 @@ def place_order(request):
             data.state = form.cleaned_data('state')
             data.city = form.cleaned_data('city')
             data.order_note = form.cleaned_data('order_note')
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            # Generate order number
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime('%Y%m%d')
             
