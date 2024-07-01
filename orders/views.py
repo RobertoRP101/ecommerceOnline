469896f51299d492a13f89f709baa0eff5ae4b49
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-01 12:15:52 -0600

    store all the billing information inside order table

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from carts.models import CartItem
-# Create your views here.
+from .forms import OrderForm
+
 
 def place_order(request):
     current_user = request.user
@@ -13,3 +14,16 @@ def place_order(request):
     
     if request.method == 'POST':
         form = OrderForm(request.POST)
+        if form.is_valid():
+            data = Order()
+            data.first_name = form.cleaned_data('first_name')
+            data.last_name = form.cleaned_data('last_name')
+            data.phone = form.cleaned_data('phone')
+            data.email = form.cleaned_data('email')
+            data.address_line_1 = form.cleaned_data('address_line_1')
+            data.address_line_2  = form.cleaned_data('address_line_2 ')
+            data.country = form.cleaned_data('country')
+            data.state = form.cleaned_data('state')
+            data.city = form.cleaned_data('city')
+            data.order_note = form.cleaned_data('order_note')
+            
