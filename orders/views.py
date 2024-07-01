d959b5bc9859434da8f4b015aac0d091530320bf
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-01 14:15:13 -0600

    changing dict for list

--- orders/views.py
+++ orders/views.py
@@ -25,16 +25,16 @@ def place_order(request, total=0, quantity=0,):
         form = OrderForm(request.POST)
         if form.is_valid():
             data = Order()
-            data.first_name = form.cleaned_data('first_name')
-            data.last_name = form.cleaned_data('last_name')
-            data.phone = form.cleaned_data('phone')
-            data.email = form.cleaned_data('email')
-            data.address_line_1 = form.cleaned_data('address_line_1')
-            data.address_line_2  = form.cleaned_data('address_line_2 ')
-            data.country = form.cleaned_data('country')
-            data.state = form.cleaned_data('state')
-            data.city = form.cleaned_data('city')
-            data.order_note = form.cleaned_data('order_note')
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2  = form.cleaned_data['address_line_2 ']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
             data.order_total = grand_total
             data.tax = tax
             data.ip = request.META.get('REMOTE_ADDR')
