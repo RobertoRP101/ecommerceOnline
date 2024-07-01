07df132891d2bfbb1f148f104a450c984742b4cc
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-06-30 23:52:18 -0600

    redirect if cart is less than or equal to 0

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+from carts.models import CartItem
 # Create your views here.
 
 def place_order(request):
-    return HttpResponse('Hello')
+    current_user = request.user
+    
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
