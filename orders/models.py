a274bde97d28df8594297d90d562ef51bcce143f
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-02 01:26:32 -0600

    full name function on models

--- orders/models.py
+++ orders/models.py
@@ -44,6 +44,9 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
     def __str__(self) -> str:
         return self.first_name
     
