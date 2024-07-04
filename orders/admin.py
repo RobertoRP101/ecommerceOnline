565f7bb62df82b4fc5156d7f3335b6208d365f24
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-04 01:32:23 -0600

    order admin class in admin on orders app

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Payment, Order, OrderProduct
 # Register your models here.
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_total', 'tax', 'status', 'is_ordered', 'created_at']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name', 'last_name', 'phone', 'email']
+    list_per_page = 20
+
 admin.site.register(Payment)
 admin.site.register(Order)
 admin.site.register(OrderProduct)
