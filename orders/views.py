69e8bf8864485035d161ddfcb1535063de75fdb7
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-04 01:03:26 -0600

    store transaction details inside payment model

--- orders/views.py
+++ orders/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm
-from .models import Order
+from .models import Order, Payment
 import datetime
 import json
 
 def payments(request):
     body = json.loads(request.body)
-    print(body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+    
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.total,
+        status = body['status'],
+    )
+    payment.save()
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+    
     return render(request, 'orders/payments.html')
 
 def place_order(request, total=0, quantity=0,):
