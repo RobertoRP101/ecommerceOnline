9ade8ad14b8edfcfee9ef4a8701982633a366a20
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-07 22:45:37 -0600

    attributes added to review rating class

--- store/models.py
+++ store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from category.models import Category
 from django.urls import reverse
-
+from accounts.models import Account
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
@@ -46,4 +46,12 @@ class Variation(models.Model):
              return self.variation_value
          
 class ReviewRating(models.Model):
-    pass
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
