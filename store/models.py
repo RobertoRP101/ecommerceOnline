fef285af1aadd0365cacc49f8bdd95377fac9960
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-12 23:54:24 -0600

    importing avg library

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from category.models import Category
 from django.urls import reverse
 from accounts.models import Account
+from django.db.models import Avg
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
