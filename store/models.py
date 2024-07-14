962fa44f34e6b205052de404728620d7bc6eca3f
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-13 23:00:22 -0600

    count review function in Product class

--- store/models.py
+++ store/models.py
@@ -28,6 +28,14 @@ class Product(models.Model):
         if reviews['average'] is not None:
             avg = float(reviews['average'])
         return avg
+    
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
+        
    
 class VariationManager(models.Manager):
     def color(self):
