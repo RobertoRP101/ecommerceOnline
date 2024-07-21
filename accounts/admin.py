b819b8ded4018d09a47a1bb37f732f93da795890
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-20 23:56:13 -0600

    issues fixed and userprofileadmin class registered

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,10 +12,11 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
     
-class UserProfile(admin.ModelAdmin):
+class UserProfileAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
         return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object,profile_picture.url))
     thumbnail.short_description = 'Profile Picture'
     list_display = ('thumbnail', 'user', 'city', 'state', 'country')
 
 admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfileAdmin, AccountAdmin)
