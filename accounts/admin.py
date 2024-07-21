8996aedc6fe93a9d32fa46fc36c03fdc849fb8cb
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-20 23:54:42 -0600

    userprofile class created on admin file in accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from .models import Account, UserProfile
 # Register your models here.
 
 class AccountAdmin(UserAdmin):
@@ -12,5 +12,10 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
     
+class UserProfile(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object,profile_picture.url))
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
 
 admin.site.register(Account, AccountAdmin)
