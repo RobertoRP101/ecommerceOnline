963fe10a87f2ac4b61207fa31a659f5df88504e1
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-21 22:46:56 -0600

    edit profile path created

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('my_orders/', views.my_orders, name='my_orders'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
     
 ]
