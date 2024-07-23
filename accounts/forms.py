01c51a985ac4027afd226dc9ede6a4653b4147f6
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-22 23:43:22 -0600

    userprofileform class added to forms on accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -36,3 +36,8 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ('first_name', 'last_name', 'phone_number')
+        
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('address_line_1', 'address_line_2', 'city', 'state', 'country', 'profile_picture')
