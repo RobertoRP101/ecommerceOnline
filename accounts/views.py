27d4977b3c4bf1dd24ec875d2301d81f783b8c82
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-27 23:45:03 -0600

    subtotal viriable sent to order_Detail page and minors corrections at information displayed

--- accounts/views.py
+++ accounts/views.py
@@ -266,8 +266,12 @@ def change_password(request):
 def order_detail(request, order_id):
     order_detail = OrderProduct.objects.filter(order__order_number=order_id)
     order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
     context = {
         'order_detail': order_detail,
         'order': order,
+        'subtotal': subtotal,
     }
     return render (request, 'accounts/order_detail.html', context)
