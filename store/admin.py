4fd3bb98bcc7c193a71f9c5dc47b32f451f0382a
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-07-30 23:28:05 -0600

    image added to admin_thumbnails decorator

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Product, Variation, ReviewRating, ProductGallery
 import admin_thumbnails
 
-@admin_thumbnails
+@admin_thumbnails.thumbnail('image')
 class ProductGalleryInline(admin.TabularInline):
     model = ProductGallery
     extra = 1
