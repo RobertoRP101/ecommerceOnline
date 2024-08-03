017f3a253cfc9776c0f2f014120236bc29834659
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-02 23:30:06 -0600

    reviewrating class imported to greatkart views file

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, ReviewRating
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
